3081f4a13848177d07fcf1d63694b05a984d3b44
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-09 16:39:05 +0300

    button dinamik props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Products from "./components/Products/Products";
+import Button from "./components/UI/Button";
 
 function App() {
   return (
     <React.Fragment>
       <h1>Hello, world!</h1>
       <Products></Products>
+      <br />
+      <Button title="Sil" addClass="danger" />
+      <Button title="Kaydet" addClass="success" />
+      <Button title="Düzenle" addClass="warning" />
     </React.Fragment>
   );
 }
