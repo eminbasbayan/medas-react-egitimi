a167f1089a9b9bc2084cc34505617cc6ff4f782b
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-09 15:22:34 +0300

    dinamik props

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,45 +5,32 @@ const productData = [
   {
     id: 1,
     productTitle: "Elma",
-    imageUrl: "https://images.unsplash.com/photo-1646052788724-7077147686c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    productPrice: 10
-  }
-]
+    imageUrl:
+      "https://images.unsplash.com/photo-1611529695235-cbe25cddec4a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    productPrice: 10,
+  },
+  {
+    id: 2,
+    productTitle: "Üzüm",
+    imageUrl:
+      "https://images.unsplash.com/photo-1646052788724-7077147686c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    productPrice: 30,
+  },
+];
 
 function Products() {
-  const imageUrl =
-    "https://images.unsplash.com/photo-1611529695235-cbe25cddec4a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-  const productTitle = "Elma";
-  const productPrice = 10;
-
   return (
     <div className="products">
-      <ProductItem
-        imageUrl={imageUrl}
-        productTitle={productTitle}
-        productPrice={productPrice}
-        name="Emin"
-      ></ProductItem>
-      <ProductItem
-        imageUrl="https://images.unsplash.com/photo-1646052788724-7077147686c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        productTitle="Üzüm"
-        productPrice={30}
-      ></ProductItem>
-      <ProductItem
-        imageUrl="https://images.unsplash.com/photo-1646052788724-7077147686c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        productTitle="Üzüm"
-        productPrice={30}
-      ></ProductItem>
-      <ProductItem
-        imageUrl="https://images.unsplash.com/photo-1646052788724-7077147686c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        productTitle="Üzüm"
-        productPrice={30}
-      ></ProductItem>
-      <ProductItem
-        imageUrl="https://images.unsplash.com/photo-1646052788724-7077147686c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        productTitle="Üzüm"
-        productPrice={30}
-      ></ProductItem>
+      {productData.map(function (product) {
+        return (
+          <ProductItem
+            imageUrl={product.imageUrl}
+            productTitle={product.productTitle}
+            productPrice={product.productPrice}
+            key={product.id}
+          />
+        );
+      })}
     </div>
   );
 }
