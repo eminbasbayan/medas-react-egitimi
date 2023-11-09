68c287ba7128264af7f74e59a93b148fac779d0b
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-09 15:32:04 +0300

    componentlerde js dosyası kullanımı

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,22 +1,6 @@
 import ProductItem from "./ProductItem";
 import "./Products.css";
-
-const productData = [
-  {
-    id: 1,
-    productTitle: "Elma",
-    imageUrl:
-      "https://images.unsplash.com/photo-1611529695235-cbe25cddec4a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    productPrice: 10,
-  },
-  {
-    id: 2,
-    productTitle: "Üzüm",
-    imageUrl:
-      "https://images.unsplash.com/photo-1646052788724-7077147686c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    productPrice: 30,
-  },
-];
+import { productData } from "../../productData";
 
 function Products() {
   return (
