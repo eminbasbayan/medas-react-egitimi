cbfffd0551296776382a3248c736fe66fa136c0e
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-10 10:14:32 +0300

    useState örnek

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Counter() {
   const number = 5;
   const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("Ahmet");
   //   let counter = 0;
 
   function arttir() {
@@ -15,10 +16,16 @@ function Counter() {
     setCounter(counter - number);
   }
 
+  function ismiDegistir() {
+    setName(name === "Ahmet" ? "Mehmet" : "Ahmet");
+  }
+
   console.log(counter);
   return (
     <div className="counter">
       <h2>Counter Component</h2>
+      <h3>{name}</h3>
+      <button onClick={ismiDegistir}>İsmi Değiştir</button>
       <button onClick={arttir}>Arttır</button>
       <strong>{counter}</strong>
       <button onClick={azalt}>Azalt</button>
