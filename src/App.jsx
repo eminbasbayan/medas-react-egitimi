a3d38e9fe701036455edcbafa6e3178f8fff7d65
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-10 15:57:59 +0300

    useEffect hook tanımlama

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import Products from "./components/Products/Products";
 
 function App() {
+  const [name, setName] = useState("Ahmet");
+  const [counter, setCounter] = useState(0);
+
+  // useEffect(() => {
+  //   console.log("ilk yüklendiğinde çalışsın. bir daha çalışmasın");
+  // }, []);
+
+  useEffect(() => {
+    console.log("state e bağlı çalışsın");
+  }, [counter]);
+
   return (
     <div className="container mx-auto">
+      <b>{name}</b>
+      <button onClick={() => setName(name === "Ahmet" ? "Mehmet" : "Ahmet")}>
+        Değiştir
+      </button>
+      <br />
+      <b>{counter}</b>
+      <button onClick={() => setCounter(counter + 1)}>Arttır</button>
       <Products />
     </div>
   );
