24d631662e83de7cb6a77ce0a090264627225a59
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-11 13:19:24 +0300

    header oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import Products from "./components/Products/Products";
+import Header from "./components/UI/Header";
 
 function App() {
   return (
     <div className="container mx-auto">
+      <Header />
       <Products />
     </div>
   );
