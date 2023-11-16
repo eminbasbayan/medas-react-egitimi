b3af43773553b5c9813d05441027e38e0f894fac
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-16 10:56:51 +0300

    link ile sayfalar arasında gezinme

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,16 @@ import {
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <HomePage />,
-//   },
-//   {
-//     path: "/products",
-//     element: <ProductsPage />,
-//   },
-// ]);
-
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
-
-const router = createBrowserRouter(routeDefinitions);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/products",
+    element: <ProductsPage />,
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,15 @@
 import Header from "../components/Layout/Header";
-
+import { Link } from "react-router-dom";
 function HomePage() {
   return (
     <>
       <Header />
-      <h1>My Home Page</h1>
+      <div className="container mx-auto">
+        <h1>My Home Page</h1>
+        <a href={"/products"}>Go to Products Page</a>
+        <br />
+        <Link to={"/products"}>Go to Products Page</Link>
+      </div>
     </>
   );
 }
