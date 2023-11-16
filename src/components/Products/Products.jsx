ef3ebce557e816df91531b02dce2dbe191fe83c5
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-16 13:27:29 +0300

    product component oluşturuldu

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,11 @@
+import ProductItem from './ProductItem'
+
+const Products = () => {
+  return (
+    <div className='products'>
+        <ProductItem />
+    </div>
+  )
+}
+
+export default Products
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
+import Products from "../components/Products/Products";
 
 function ProductsPage() {
   return (
     <Fragment>
       <h1>My Products Page</h1>
+      <Products />
     </Fragment>
   );
 }
