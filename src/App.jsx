c3194d5fb380227f6b9a9bbb7b4f7637d7ed901a
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-16 09:30:21 +0300

    yeni proje ayarları

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import { useContext, useEffect, useState } from "react";
-import Products from "./components/Products/Products";
-import Header from "./components/UI/Header";
-import { ThemeContext } from "./context/ThemeContext";
-
 function App() {
-  const { themeMode } = useContext(ThemeContext);
   return (
-    <div
-      className={`container mx-auto ${
-        themeMode === "Dark" && "bg-black text-white"
-      } `}
-    >
-      <Header />
-      <Products />
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,4 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import CartProvider from "./context/CartContext.jsx";
-import ThemeProvider from "./context/ThemeContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <CartProvider>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </CartProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
