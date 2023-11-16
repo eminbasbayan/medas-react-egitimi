38c53e46e5caa05263863e02f67afc9cea14f77f
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-16 11:50:04 +0300

    useNavigate hook kullanımı

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => navigate("/products");
+
   return (
     <div className="bg-gray-200 w-full px-16 md:px-0 h-screen flex items-center justify-center">
       <div className="bg-white border border-gray-200 flex flex-col items-center justify-center px-4 md:px-8 lg:px-24 py-8 rounded-lg shadow-2xl">
@@ -13,8 +18,8 @@ const ErrorPage = () => {
         <p className="text-gray-500 mt-4 pb-4 border-b-2 text-center">
           Sorry, the page you are looking for could not be found.
         </p>
-        <a
-          href="/"
+        <button
+          onClick={handleNavigate}
           className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 mt-6 rounded transition duration-150"
           title="Return Home"
         >
@@ -31,7 +36,7 @@ const ErrorPage = () => {
             ></path>
           </svg>
           <span>Return Home</span>
-        </a>
+        </button>
       </div>
     </div>
   );
