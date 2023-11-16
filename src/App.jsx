10e5393f554fa2e5f8c649ce055483084faaa0b7
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-16 09:59:50 +0300

    createBrowserRouter ile ilk route tanımlama

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import Header from "./components/Layout/Header";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      <HomePage />
-      <ProductsPage />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
