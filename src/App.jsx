3dd7db1a27abc5a8728e66b42de67ccb02094425
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-16 15:15:40 +0300

    react router components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import {
-  Route,
   RouterProvider,
   createBrowserRouter,
-  createRoutesFromElements,
+  BrowserRouter,
+  Routes,
+  Route,
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
@@ -10,33 +11,19 @@ import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/ErrorPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/products",
-        element: <ProductsPage />,
-      },
-      {
-        path: "/products/:productId",
-        element: <ProductDetailsPage />,
-      },
-      {
-        path: "*",
-        element: <ErrorPage />,
-      },
-    ],
-  },
-]);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:productId" element={<ProductDetailsPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
