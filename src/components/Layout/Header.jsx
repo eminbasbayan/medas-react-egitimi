192f99f7a06d07a9f6c24c3f934b28ce304818ab
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-16 12:06:10 +0300

    NavLink classNames düzenleme

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,6 +1,9 @@
 import { Link, NavLink } from "react-router-dom";
 
 function Header() {
+  const navLinkClassNames =
+    "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700";
+
   return (
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
@@ -80,7 +83,9 @@ function Header() {
                 <NavLink
                   to="/"
                   className={({ isActive }) =>
-                    isActive ? "text-blue-500" : ""
+                    isActive
+                      ? `${navLinkClassNames} !text-blue-500`
+                      : navLinkClassNames
                   }
                 >
                   Ana Sayfa
@@ -90,7 +95,9 @@ function Header() {
                 <NavLink
                   to="/products"
                   className={({ isActive }) =>
-                    isActive ? "text-blue-500" : ""
+                    isActive
+                      ? `${navLinkClassNames} !text-blue-500`
+                      : navLinkClassNames
                   }
                 >
                   Ürünler
