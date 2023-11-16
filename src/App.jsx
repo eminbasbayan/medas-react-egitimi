559c72e6143168588da15013c9f98dedc32af653
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-16 09:36:54 +0300

    header oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./components/Layout/Header";
+
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Header />
+    </div>
+  );
 }
 
-export default App
+export default App;
