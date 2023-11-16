0d227e5f27084775dfb0eb2f1cb96e9beabb808d
Emin Başbayan <48470345+eminbasbayan@users.noreply.github.com>
2023-11-16 09:49:38 +0300

    home page oluşturuldu, react router dom yüklendi

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Layout/Header";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <h1>My Home Page</h1>
+    </>
+  );
+}
+
+export default HomePage;
